514ee7c11bcd92e77f3c951572a54067c785f768
Alexandre <alexandrems1000@hotmail.com>
2016-06-17 15:13:05 +0100

    MODIFICAÇÃO CANAIS TEMPORARIOS
    LISTA INSPRATION

--- plugin.video.tvalex/addon.py
+++ plugin.video.tvalex/addon.py
@@ -18,11 +18,11 @@
 
 ##############BIBLIOTECAS A IMPORTAR E DEFINICOES####################
 
-import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmc,xbmcaddon,HTMLParser,base64,xmltosrt,os,sys,time
+import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmc,xbmcaddon,HTMLParser,base64,xmltosrt,os,sys,time,re,os,unicodedata
 from BeautifulSoup import BeautifulStoneSoup, BeautifulSoup, BeautifulSOAP
 h = HTMLParser.HTMLParser()
 
-ultimaversao = '0.3.7'
+ultimaversao = '0.3.8'
 addon_id = 'plugin.video.tvalex'
 selfAddon = xbmcaddon.Addon(id=addon_id)
 addonfolder = selfAddon.getAddonInfo('path')
@@ -33,6 +33,25 @@ base_url = 'http://www.tuga-filmes.com/'
 
 ################################################## 
 
+plugin_handle = int(sys.argv[1])
+
+mysettings = xbmcaddon.Addon(id = 'plugin.video.Premium_Inspiration')
+profile = mysettings.getAddonInfo('profile')
+home = mysettings.getAddonInfo('path')
+fanart = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
+icon = xbmc.translatePath(os.path.join(home, 'icon.png'))
+
+online_m3u = mysettings.getSetting('online_m3u')
+local_m3u = mysettings.getSetting('local_m3u')
+online_xml = mysettings.getSetting('online_xml')
+local_xml = mysettings.getSetting('local_xml')
+
+xml_regex = '<title>(.*?)</title>\s*<link>(.*?)</link>\s*<thumbnail>(.*?)</thumbnail>'
+m3u_thumb_regex = 'tvg-logo=[\'"](.*?)[\'"]'
+m3u_regex = '#(.+?),(.+)\s*(.+)\s*'
+
+u_tube = 'http://www.youtube.com'
+
 
 #MENUS############################################
 
@@ -41,7 +60,7 @@ def menus():
 	#dialog.ok("TV BOX ALEX", "[COLOR red]                             MENSAGEM IMPORTANTE:                             [/COLOR][COLOR white]CAROS CLIENTES E AMIGOS, O ADDON ESTA EM ATUALIZAÇÃO, QUALQUER ERRO PEÇO DESCULPAS, SEREI BREVE. OBRIGADO[/COLOR]                              [COLOR GREEN]CONSULTAS:[/COLOR]                                      [COLOR YELLOW]FACEBOOK: Tv Box Canais Internacionais.[/COLOR]")
 	addDir('[B]CANAIS TUGA[/B]','-',3,artfolder + 'CANAISTUGA.png')
 	addDir('[B]CANAIS DO BRASIL[/B]','-',9,artfolder + 'CANAISBRASIL.png')
-	addDir('[B]C. TEMPORARIOS[/B]','https://dl.dropbox.com/s/rsedumwxfcprs55/CANAISTEMP.txt?dl=0',4,artfolder + 'TEMPORARIOS.png')
+	addDir('[B]TEMPORARIOS BY INSPIRATION[/B]','-',100,artfolder + 'TEMPORARIOS.png')
 	addDir('[B]UK TV[/B]','https://dl.dropbox.com/s/wxloawcl620fo8s/UKTV.txt?dl=0',4,artfolder + 'CANAISUK.png')
 	addDir('[B]CANAIS INFANTIS[/B]','https://dl.dropbox.com/s/u4d5b3hbo2cd8va/Infantil.txt?dl=0',4,artfolder + 'CANAISINFANTIS.png')
 	addDir('[B]DESPORTO MUNDO[/B]','https://dl.dropbox.com/s/iuxhcbk1t5a2yzw/DesportoMundo.txt?dl=0',8,artfolder + 'ESPORTESINTERN.png')
@@ -63,8 +82,52 @@ def tvbrasil():
 	addDir('RELIGIOSOS (WEBCAM)','https://dl.dropbox.com/s/t4q1p88rnwxakzj/Religiosos.txt?dl=0',8,artfolder + 'RELIGIOSOS.png')
 	xbmc.executebuiltin("Container.SetViewMode(500)")
 
+def main():
+	add_dir('[B]<<<  SEARCH  >>>[/B]', 'searchlink', 99, icon, fanart)
+	if len(online_m3u) > 0:	
+		add_dir('[COLOR yellow][B]>> ONLINE M3U <<[/B][/COLOR]', u_tube, 102, icon, fanart)
+	if len(local_m3u) > 0:	
+		add_dir('[COLOR magenta][B]>> LOCAL M3U <<[/B][/COLOR]', u_tube, 103, icon, fanart)
+	if len(online_xml) > 0:	
+		add_dir('[COLOR cyan][B]>> ONLINE XML <<[/B][/COLOR]', u_tube, 104, icon, fanart)
+	if len(local_xml) > 0:	
+		add_dir('[COLOR lime][B]>> LOCAL XML <<[/B][/COLOR]', u_tube, 105, icon, fanart)		
+	if (len(online_m3u) < 1 and len(local_m3u) < 1 and len(online_xml) < 1 and len(local_xml) < 1 ):		
+		mysettings.openSettings()
+		xbmc.executebuiltin("Container.Refresh")
 
 ###################################################################################
+
+def removeAccents(s):
+	return ''.join((c for c in unicodedata.normalize('NFD', s.decode('utf-8')) if unicodedata.category(c) != 'Mn'))
+					
+def read_file(file):
+    try:
+        f = open(file, 'r')
+        content = f.read()
+        f.close()
+        return content
+    except:
+        pass
+
+def make_request1(url):
+	try:
+		req = urllib2.Request(url)
+		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0')
+		response = urllib2.urlopen(req)	  
+		link = response.read()
+		response.close()  
+		return link
+	except urllib2.URLError, e:
+		print 'We failed to open "%s".' % url
+		if hasattr(e, 'code'):
+			print 'We failed with error code - %s.' % e.code	
+		if hasattr(e, 'reason'):
+			print 'We failed to reach a server.'
+			print 'Reason: ', e.reason
+
+###################################################################################
+
 #FUNCOES
 
 def listar_canais(url):
@@ -118,6 +181,9 @@ def versao_disponivel():
 	versao=ultimaversao
 	addDir('[B][COLOR blue]Definições do Add-on[/COLOR][/B]','-',11,artfolder + 'definicoes.png',False)
 	addLink('[B][COLOR white]Versão (' + versao + ')[/COLOR][/B]','',artfolder + 'versao.png')
+	
+
+	
 			
 ################################################
 #    Funções relacionadas a media.             #
@@ -192,6 +258,14 @@ def addLink(name,url,iconimage,plot=''):
 	liz.setInfo( type="Video", infoLabels={ "Title": name, "plot": plot } )
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
 	return ok
+	
+def add_link1(name, url, mode, iconimage, fanart):
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)	
+	liz = xbmcgui.ListItem(name, iconImage = "DefaultVideo.png", thumbnailImage = iconimage)
+	liz.setInfo( type = "Video", infoLabels = { "Title": name } )
+	liz.setProperty('fanart_image', fanart)
+	liz.setProperty('IsPlayable', 'true') 
+	ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = liz)
 
 def addDir(name,url,mode,iconimage,pasta=True,total=1,plot=''):
 	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
@@ -200,6 +274,19 @@ def addDir(name,url,mode,iconimage,pasta=True,total=1,plot=''):
 	liz.setProperty('fanart_image', fanart)
 	liz.setInfo( type="video", infoLabels={ "title": name, "plot": plot } )
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=pasta,totalItems=total)
+	return ok
+	
+def add_dir(name, url, mode, iconimage, fanart):
+	u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name) + "&iconimage=" + urllib.quote_plus(iconimage)
+	ok = True
+	liz = xbmcgui.ListItem(name, iconImage = "DefaultFolder.png", thumbnailImage = iconimage)
+	liz.setInfo( type = "Video", infoLabels = { "Title": name } )
+	liz.setProperty('fanart_image', fanart)
+	if ('youtube.com/user/' in url) or ('youtube.com/channel/' in url) or ('youtube/user/' in url) or ('youtube/channel/' in url):
+		u = 'plugin://plugin.video.youtube/%s/%s/' % (url.split( '/' )[-2], url.split( '/' )[-1])
+		ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = liz, isFolder = True)
+		return ok		
+	ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = liz, isFolder = True)
 	return ok
 
 def videomega_resolver(referer):
@@ -263,7 +350,124 @@ def addDirPlayer(name,url,mode,iconimage,total,fnart):
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False,totalItems = total)
 	return ok
 	
+def m3u_online():		
+	content = make_request1(online_m3u)
+	match = re.compile(m3u_regex).findall(content)
+	for thumb, name, url in match:
+		try:
+			m3u_playlist(name, url, thumb)
+		except:
+			pass
+			
+def xml_online():			
+	content = make_request1(online_xml)
+	match = re.compile(xml_regex).findall(content)
+	for name, url, thumb in match:
+		try:
+			xml_playlist(name, url, thumb)
+		except:
+			pass
+			
+def m3u_local():
+	content = read_file(local_m3u)
+	match = re.compile(m3u_regex).findall(content)
+	for thumb, name, url in match:	
+		try:
+			m3u_playlist(name, url, thumb)
+		except:
+			pass
 
+def xml_local():		
+	content = read_file(local_xml)
+	match = re.compile(xml_regex).findall(content)
+	for name, url, thumb in match:	
+		try:
+			xml_playlist(name, url, thumb)
+		except:
+			pass
+			
+def m3u_playlist(name, url, thumb):	
+	name = re.sub('\s+', ' ', name).strip()			
+	url = url.replace('"', ' ').replace('&amp;', '&').strip()
+	if ('youtube.com/user/' in url) or ('youtube.com/channel/' in url) or ('youtube/user/' in url) or ('youtube/channel/' in url):
+		if 'tvg-logo' in thumb:
+			thumb = re.compile(m3u_thumb_regex).findall(str(thumb))[0].replace(' ', '%20')			
+			add_dir(name, url, '', thumb, thumb)			
+		else:	
+			add_dir(name, url, '', icon, fanart)
+	else:
+		if 'youtube.com/watch?v=' in url:
+			url = 'plugin://plugin.video.youtube/play/?video_id=%s' % (url.split('=')[-1])
+		elif 'dailymotion.com/video/' in url:
+			url = url.split('/')[-1].split('_')[0]
+			url = 'plugin://plugin.video.dailymotion_com/?mode=playVideo&url=%s' % url	
+		else:			
+			url = url
+		if 'tvg-logo' in thumb:				
+			thumb = re.compile(m3u_thumb_regex).findall(str(thumb))[0].replace(' ', '%20')
+			add_link1(name, url, 1, thumb, thumb)			
+		else:				
+			add_link1(name, url, 1, icon, fanart)	
+					
+def xml_playlist(name, url, thumb):
+	name = re.sub('\s+', ' ', name).strip()			
+	url = url.replace('"', ' ').replace('&amp;', '&').strip()
+	if ('youtube.com/user/' in url) or ('youtube.com/channel/' in url) or ('youtube/user/' in url) or ('youtube/channel/' in url):
+		if len(thumb) > 0:	
+			add_dir(name, url, '', thumb, thumb)			
+		else:	
+			add_dir(name, url, '', icon, fanart)
+	else:
+		if 'youtube.com/watch?v=' in url:
+			url = 'plugin://plugin.video.youtube/play/?video_id=%s' % (url.split('=')[-1])
+		elif 'dailymotion.com/video/' in url:
+			url = url.split('/')[-1].split('_')[0]
+			url = 'plugin://plugin.video.dailymotion_com/?mode=playVideo&url=%s' % url	
+		else:			
+			url = url
+		if len(thumb) > 0:		
+			add_link1(name, url, 1, thumb, thumb)			
+		else:			
+			add_link1(name, url, 1, icon, fanart)	
+			
+def play_video(url):
+	media_url = url
+	item = xbmcgui.ListItem(name, path = media_url)
+	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+	return
+	
+def search(): 	
+	try:
+		keyb = xbmc.Keyboard('', 'Enter search text')
+		keyb.doModal()
+		if (keyb.isConfirmed()):
+			searchText = urllib.quote_plus(keyb.getText()).replace('+', ' ')
+		if len(online_m3u) > 0:		
+			content = make_request(online_m3u)
+			match = re.compile(m3u_regex).findall(content)
+			for thumb, name, url in match:
+				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
+					m3u_playlist(name, url, thumb)	
+		if len(local_m3u) > 0:		
+			content = read_file(local_m3u)
+			match = re.compile(m3u_regex).findall(content)		
+			for thumb, name, url in match:
+				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
+					m3u_playlist(name, url, thumb)	
+		if len(online_xml) > 0:					
+			content = make_request(online_xml)
+			match = re.compile(xml_regex).findall(content)	
+			for name, url, thumb in match:
+				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
+					xml_playlist(name, url, thumb)	
+		if len(local_xml) > 0:		
+			content = read_file(local_xml)
+			match = re.compile(xml_regex).findall(content)		
+			for name, url, thumb in match:
+				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
+					xml_playlist(name, url, thumb)	
+	except:
+		pass
 
 ############################################################################################################
 #                                               GET PARAMS                                                 #
@@ -339,6 +543,22 @@ elif mode==9:
 elif mode==10:
 	selfAddon.openSettings()
 	
+elif mode==100:
+	main()	
 	
+elif mode == 102:
+	m3u_online()
+	
+elif mode == 103:
+	m3u_local()
+	
+elif mode == 104:
+	xml_online()
+	
+elif mode == 105:
+	xml_local()	
+
+elif mode == 99:
+	search()
 	
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
